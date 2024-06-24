2c8d72519305c573a4d0baedaaec193d743fd1d9
Sharan6214 <sharan6214@gmail.com>
2024-06-24 19:20:34 +0530

    Create color.dart

---
+++ color.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  List<String> colorList = ["Red", "Green", "White", "Black"];
+
+  String firstColor = colorList.first;
+ 
+  String lastColor = colorList.last;
+
+  print('First color: $firstColor');
+  print('Last color: $lastColor');
+}
+
+
